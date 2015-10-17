81eb995d99b5b362469ddbc1bb458dfb9d0556e9
bgrffqq <su3g4284zo6y7@gmail.com>
2015-10-17 20:13:09 +0800

    modify settings

--- Spotify_Visualized/production_settings.py
+++ Spotify_Visualized/production_settings.py
@@ -2,8 +2,13 @@
 from .settings import *
 import os
 
+import dj_database_url
+DATABASES = {
+        'default': dj_database_url.config()
+}
+
 # Static asset configuration
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+STATIC_ROOT = "staticfiles"
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
